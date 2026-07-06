2be3d65a425605ac1118a92dc05da60d01c72ed6
dpsv13 <23bq1a1225@vvit.net>
2026-07-06 13:18:37 +0530

    Added Sum program

---
+++ Sum.java
@@ -0,0 +1,17 @@
+public class Sum {
+    public static void main(String[] args) {
+
+        if (args.length == 2) {
+
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+
+            System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+
+        } else {
+
+            System.out.println("Please pass two integers.");
+
+        }
+    }
+}
