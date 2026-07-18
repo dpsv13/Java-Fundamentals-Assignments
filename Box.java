a438cb7108276cd2a72908cda1549604b064e6ad
dpsv13 <23bq1a1225@vvit.net>
2026-07-18 08:47:39 +0530

    Added Box program

---
+++ Box.java
@@ -0,0 +1,28 @@
+public class Box {
+
+    double width;
+    double height;
+    double depth;
+
+    // Parameterized Constructor
+    Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    // Method to calculate volume
+    double getVolume() {
+        return width * height * depth;
+    }
+
+    public static void main(String[] args) {
+
+        Box b = new Box(10, 5, 2);
+
+        System.out.println("Width : " + b.width);
+        System.out.println("Height : " + b.height);
+        System.out.println("Depth : " + b.depth);
+        System.out.println("Volume : " + b.getVolume());
+    }
+}
