75d01962ed2e3a6cee3bb01143b1946d26c2339a
dpsv13 <23bq1a1225@vvit.net>
2026-07-22 09:23:39 +0530

    Added ArrayList StringJoiner program

---
+++ ArrayListStringJoiner.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class ArrayListStringJoiner {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> names = new ArrayList<>();
+
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter names:");
+
+        for (int i = 0; i < n; i++) {
+            names.add(sc.next());
+        }
+
+        StringJoiner sj = new StringJoiner(", ", "{", "}");
+
+        for (String name : names) {
+            sj.add(name);
+        }
+
+        System.out.println(sj);
+
+        sc.close();
+    }
+}
